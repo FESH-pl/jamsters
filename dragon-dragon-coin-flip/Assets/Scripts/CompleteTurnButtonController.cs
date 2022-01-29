322b985bc16ddd803f9c63fa00843fdbca31a993
FESH-pl <94991777+FESH-pl@users.noreply.github.com>
2022-01-29 12:09:36 +0100

    Added States and UI Canvas object

--- dragon-dragon-coin-flip/Assets/Scripts/CompleteTurnButtonController.cs
+++ dragon-dragon-coin-flip/Assets/Scripts/CompleteTurnButtonController.cs
@@ -14,12 +14,7 @@ public class CompleteTurnButtonController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // Show button if correct state
-        // TODO: this might make more sense if we manage it in the State script and set active true/false for a parent object (recursive)
-        if (stateMachine.currentState == stateMachine.playerChoiceState)
-            gameObject.GetComponent<Renderer>().enabled = true;
-        else
-            gameObject.GetComponent<Renderer>().enabled = false;
+
     }
 
     public void OnButtonClick()
